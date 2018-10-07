37b8ade44be88fb99a4859214f98137f8b71b6a2
Nakonechna Galyna <nakonechnayag@gmail.com>
2018-10-08 00:22:03 +0300

    HW#6 unit1 1, 2, 3 tasks

--- hw/hw_1.6.py
+++ hw/hw_1.6.py
@@ -4,6 +4,7 @@
 
 print([i ** 2 for i in range(1, 100)])
 
+
 # 2
 # Make a program that prompts the user to input the name of a car, the program
 # should save the input in a list and ask for another, and then another, until
@@ -19,6 +20,7 @@ while True:
     else:
         cars.append(answer)
 print(cars)
+
 
 # 3
 # Start of with any list containing at least 10 elements, then print all
